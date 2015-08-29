5ffb380195fb1ec8ac4246daf9b8720edb84ded4
Nghia Em <nghiaem@gmail.com>
2015-08-29 12:11:05 +0700

    Add C2C request header, token value from response

--- src/kz_api_helper.erl
+++ src/kz_api_helper.erl
@@ -1,6 +1,10 @@
 -module(kz_api_helper).
 
 -export([create_header/0,create_header/1]).
+-export([get_token_request/2]).
+-export([get_response_value/2]).
+-export([ge_c2c_request/3]).
+
 
 create_header() ->
 	#{<<"Accept">> => <<"application/json">>,<<"Content-Type">> => <<"application/json">>}.
@@ -9,3 +13,19 @@ create_header(AuthenToken) ->
 	HeaderMap = create_header(),
 	HeaderMap2 = maps:put(<<"X-Auth-Token">>, AuthenToken, HeaderMap),
 	HeaderMap2.
+
+get_token_request(AccountName,Credential) ->
+	Body = jsx:encode([{<<"data">>,[{<<"credentials">>,Credential},{<<"account_name">>,AccountName}]}]),
+	Url = "/v1/user_auth",
+	{Body,Url}.
+	
+ge_c2c_request(AccountId, C2cId, Contact) ->
+	Body = jsx:encode([{<<"data">>,[{<<"contact">>,Contact}]},{<<"verb">>,<<"POST">>}]),
+	Url = "/v1/accounts/"++AccountId++"/clicktocall/"++C2cId++"/connect",
+	{Body,Url}.
+	
+get_response_value(Response,Key) -> 
+	ResBody = maps:get(body,Response),
+	ResDecode = jsx:decode(ResBody),
+	AuthToken = proplists:get_value(Key,ResDecode,<<>>),
+	AuthToken.
