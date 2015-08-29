bb3f80374b262fca721a29274a60565fc6c4532e
Nghia Em <nghiaem@gmail.com>
2015-08-29 20:26:39 +0700

    Get response value from two key levels

--- src/kz_api_helper.erl
+++ src/kz_api_helper.erl
@@ -2,7 +2,7 @@
 
 -export([create_header/0,create_header/1]).
 -export([get_token_request/2]).
--export([get_response_value/2]).
+-export([get_response_value/2,get_response_value/3]).
 -export([ge_c2c_request/3]).
 
 
@@ -27,5 +27,10 @@ ge_c2c_request(AccountId, C2cId, Contact) ->
 get_response_value(Response,Key) -> 
 	ResBody = maps:get(body,Response),
 	ResDecode = jsx:decode(ResBody),
-	AuthToken = proplists:get_value(Key,ResDecode,<<>>),
-	AuthToken.
+	Value = proplists:get_value(Key,ResDecode,<<>>),
+	Value.
+	
+get_response_value(Response, ParentKey, ChildKey) -> 
+	ParentValue = get_response_value(Response,ParentKey),
+	Value = proplists:get_value(ChildKey,ParentValue,<<>>),
+	Value.
