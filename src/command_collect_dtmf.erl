0aece123e90a6ca9fc7ca48faee0fe62cc80aaf2
Nghia Em <nghiaem@gmail.com>
2015-11-13 01:24:11 +0700

    Add default parameters: timeout, interdigits_timeout, terminator

--- src/command_collect_dtmf.erl
+++ src/command_collect_dtmf.erl
@@ -1,3 +1,4 @@
+% Reference this module: https://github.com/2600hz/kazoo/blob/305053c762795ba279028c82c5e83b25f4d8901e/applications/callflow/src/module/cf_collect_dtmf.erl
 -module(command_collect_dtmf).
 
 -export([create/1,create/2,create_command_collect_dtmf/1, create_command_collect_dtmf/2]).
@@ -9,7 +10,7 @@ create(CollectionName,MaxDigits) ->
 	create_command_collect_dtmf(CollectionName,MaxDigits).
 	
 create_command_collect_dtmf(CollectionName) ->
-[{<<"module">>,<<"collect_dtmf">>},{<<"data">>,[{<<"collection_name">>,CollectionName},{<<"max_digits">>,4}]}].
+[{<<"module">>,<<"collect_dtmf">>},{<<"data">>,[{<<"collection_name">>,CollectionName},{<<"max_digits">>,4},{<<"timeout">>,5000},{<<"interdigit_timeout">>,2000},{<<"terminator">>,<<"#">>}]}].
 
 create_command_collect_dtmf(CollectionName,MaxDigits) ->
 [{<<"module">>,<<"collect_dtmf">>},{<<"data">>,[{<<"collection_name">>,CollectionName},{<<"max_digits">>,MaxDigits}]}].
